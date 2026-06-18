2bdbf9fb87ade0576efcf14387e2cd96677194e2
Tapas Kumar Mohanty <tapasmahanty2006@gmail.com>
2026-06-18 07:27:49 +0530

    CLONE WEBSITE

---
+++ t.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+t.left(85)
+while True:
+    t.right(170)
+    t.forward(400)
+    t.left(170)
+    t.forward(400)
+    t.speed('fastest')
+    t.pensize(10)
+    turtle.bye()
+
+
+# import string as ammo
+# gun = list(ammo.ascii_uppercase)
+# print(gun.pop())
